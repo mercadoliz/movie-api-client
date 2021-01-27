0e50d04b6c8861072758c44174de86549bf5c380
mercadoliz <mercadoliz08@gmail.com>
2021-01-27 12:50:34 -0500

    3.3

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import axios from 'axios';
+import {MovieCard} from '../movie-card/movie-card';
+import {MovieView} from '../movie-view/movie-view';
+
+export class MainView extends React.Component {
+  constructor() {
+    // Call the superclass constructor
+    // so React can initialize it
+    super();
+
+    // Initialize the state to an empty object so we can destructure it later
+    this.state = {
+      movies: [], 
+      selectedMovie:""
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://myflix-lizmovies.herokuapp.com/movies')
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  onMovieClick(movie) {
+    this.setState({
+      selectedMovie: movie
+    });
+  }
+
+  render() {
+    const { movies, selectedMovie } = this.state;
+
+    // Before the movies have been loaded
+    if (!movies) return <div className="main-view"/>;
+
+    return (
+     <div className="main-view">
+      {selectedMovie
+         ? <MovieView movie={selectedMovie}/>
+         : movies.map(movie => (
+           <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>
+         ))
+      }
+     </div>
+    );
+  }
+}
