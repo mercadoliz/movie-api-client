6055f0b4b6fba64e038f687ae7bbf4e86380e1d7
mercadoliz <mercadoliz08@gmail.com>
2021-03-12 18:13:02 -0500

    changed image size

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -55108,7 +55108,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59595" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52466" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
